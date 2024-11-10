40fc62fd13a1ed8ae409a68708707977ba48e95f
Alireza <alirezafarmani619@gmail.com>
2024-11-10 07:01:45 -0800

    styling todo components and add som data

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -9,11 +9,65 @@ defineProps({
 </script>
 
 <template>
-  <div>
-    <div>
-      {{ todo.title }}
+  <div class="content">
+    <div class="header">
+      <span>date: {{ todo.createdAt }}</span>
+      <div class="action">
+        <button class="material-icons delete"></button>
+        <label class="container">
+          done/pending
+          <input type="checkbox" :checked="todo.done">
+          <span class="check-mark"></span>
+        </label>
+      </div>
     </div>
+    <h1>{{ todo.title }}</h1>
+    <p>{{ todo.priority }}</p>
   </div>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="scss" scoped>
+.content {
+  border: 1px solid #0284c7;
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+}
+.header {
+  font-size: 12px;
+  background-color: #0284c7;
+  padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  button{
+    border-radius: 30px;
+  }
+  .action {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+  div .delete{
+    font-size: 20px;
+    padding: 3px;
+    background-color: #fff;
+    color: #ef4444;
+  }
+  h1{
+    padding: 1rem;
+  }
+  p {
+    padding: 10px;
+    font-weight: 300;
+    background-color: #0284c7;
+    width: 80px;
+    margin: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50px;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,13 +4,24 @@ import Warpper from '@/components/Warpper.vue';
 const todos = [
   {
     id: 1,
-    title: 'a',
+    title: 'reading book',
     done: true,
+    createdAt: "11/06/2023",
+    priority: "high"
   },
   {
     id: 2,
-    title: 'b',
+    title: 'do home works',
     done: false,
+    createdAt: "12/06/2023",
+    priority: "normal",
+  },
+  {
+    id: 3,
+    title: 'chocking',
+    done: false,
+    createdAt: "13/06/2023",
+    priority: "low",
   },
 ]
 </script>
