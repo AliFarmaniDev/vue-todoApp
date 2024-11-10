95ebcb80273e9e5ae64b77d2723ceba2be604f4a
Alireza <alirezafarmani619@gmail.com>
2024-11-09 23:17:30 -0800

    create todoItem components

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    todo: {
+        type: Object,
+        requier: true
+    }
+})
+</script>
+
+<template>
+  <div>
+    <div>
+      {{ todo.title }}
+    </div>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,27 @@
-<script setup></script>
+<script setup>
+import TodoItem from '@/components/TodoItem.vue';
+const todos = [
+  {
+    id: 1,
+    title: "a",
+    done: true
+  },
+  {
+    id: 2,
+    title: "b",
+    done: false
+  }
+]
+</script>
 
 <template>
-  <div>
-    <h2>home page</h2>
+  <div class="header">
+    <div>
+      <h3>Todos</h3>
+    </div>
+    <button>Show Todos</button>
+  </div>
+  <div v-for="todo in todos" :key="todo.id">
+    <TodoItem :todo="todo" />
   </div>
 </template>
